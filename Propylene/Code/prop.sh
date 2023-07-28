a929ce536910c4ba6047ccda2b177fc123b20f20
cm18rb <cm18rb@leeds.ac.uk>
2023-07-28 10:26:38 +0100

    added a python run file

--- Propylene/Code/prop.sh
+++ Propylene/Code/prop.sh
@@ -3,19 +3,19 @@
 
 
 
- ../Code/get_geom.x t.0
+../Code/get_geom.x t.0 #  reads in the geometries and places them inside a file called t.0 (t holds for trajectory)
 
- echo '$molecule' > f.inp
- cat geom.in >> f.inp
+ echo '$molecule' > f.inp # f.inp is the name of all the input files for qchem (f holds for forces)
+ cat geom.in >> f.inp # copy the geometry into the qchem file
  echo '$end' >> f.inp
- cat sf_diis >> f.inp
+ cat sf_diis >> f.inp # cat means copy in bash and this just copies the template of a qchem submission
     echo 'CIS_N_ROOTS' 1 >>f.inp
     echo 'CIS_STATE_DERIV' 1 >>f.inp
     echo '$end' >>f.inp
 
-    qchem -save -nt $ncpu f.inp f.out wf
+    qchem -save -nt $ncpu f.inp f.out wf # submit the qchem job
 
-    if ! grep -q "Calculating analytic gradient of the CIS energy" f.out; then
+    if ! grep -q "Calculating analytic gradient of the CIS energy" f.out; then # if the qchem job fails then resubmit under a different style
      echo '$molecule' >f.inp
      cat geom.in >>f.inp
      echo '$end' >>f.inp
@@ -24,28 +24,28 @@
      echo 'CIS_STATE_DERIV' 1 >>f.inp
      echo '$end' >>f.inp
 
-     qchem -save -nt $ncpu f.inp f.out wf
+     qchem -save -nt $ncpu f.inp f.out wf # retry the submission
     fi
-if ! grep -q "Calculating analytic gradient of the CIS energy" f.out; then
+if ! grep -q "Calculating analytic gradient of the CIS energy" f.out; then # if both methods fail then just say error and call it quits
 cat error >ERROR
 pwd >>../ERROR
 exit 1
 fi
-cat  f.out >>  f.all
-../Code/q_to_prop.x >>t.0
+cat  f.out >>  f.all # copy over the output from qchem to a list that tracks all outputs.
+../Code/q_to_prop.x >>t.0 # translates from angstroms to bohr or vice versa ? need to check again 
 
-mv t.0 t.1
- cat t.1 > t1.all
- echo '---------------------------------------------------' >>t1.all
+mv t.0 t.1 # renames t.0 to t.1 by literally moving the whole file and deletes the old one?
+ cat t.1 > t1.all # copies the data from t.1 to an overall tracker file
+ echo '---------------------------------------------------' >>t1.all # adds a separator
 
 
 
 #Propagation
 for i in {1..2500};do
 
- mv t.1 t.0
+ mv t.1 t.0 # rename the current t.1 to t.0 to begin to take a new timestep.
 
-../Code/stop.x
+../Code/stop.x # option for restarting to ensure total time is not exceeded, makes a file called stop.
 
 if [ -f stop ]; then
   break
@@ -54,11 +54,11 @@ fi
 
 
 
- ../Code/prop_prelim.x t
+ ../Code/prop_prelim.x t # I don't yet know why this takes an input of t but i guess it gives an output of t.p?
 
- ../Code/get_geom.x t.p
+ ../Code/get_geom.x t.p # translates units to bohr to be used with qchem. 
 
- echo '$molecule' > f.inp
+ echo '$molecule' > f.inp # Exact same qchem setup as before
  cat geom.in >> f.inp
  echo '$end' >> f.inp
  cat sf_diis >> f.inp
@@ -77,7 +77,7 @@ fi
      echo '$end' >>f.inp
      cat sf_gmd >>f.inp
      if [ -d wf ]; then
-       echo 'SCF_GUESS           Read' >>f.inp
+       echo 'SCF_GUESS           Read' >>f.inp 
      fi
      echo 'CIS_N_ROOTS' 1 >>f.inp
      echo 'CIS_STATE_DERIV' 1 >>f.inp
@@ -93,17 +93,17 @@ fi
 
 
 
-    cat  f.out >>  f.all
+    cat  f.out >>  f.all # same tranfer as always
 
 
 
- ../Code/q_to_prop.x >>t.p
- ../Code/prop_corr.x t
- cat t.1 >> t1.all
- echo '---------------------------------------------------' >>t1.all
- cat t.te>>te.all
+ ../Code/q_to_prop.x >>t.p # translates back to angstroms for nuclear dynamics
+ ../Code/prop_corr.x t # recalculates the momentum using the forces from both sides of the timestep 
+ cat t.1 >> t1.all #  tracking transfer
+ echo '---------------------------------------------------' >>t1.all #  separator 
+ cat t.te>>te.all #  energies stuff - maybe not important
 
-if [ "$(head -1 t.1)" != "$(head -1 t.p)" ]; then
+if [ "$(head -1 t.1)" != "$(head -1 t.p)" ]; then # if a molecule is removed because the forces are removed but records when it happens.
   rm -r wf
   echo $i>>t.diss1
 fi
---
+++ Propylene/Code/run.py
@@ -0,0 +1,217 @@
+#########################################################################################
+#
+#   Python Run script for the submission of dissociation after electron impact
+#   Written by R Brook                                                 26.07.23
+#   Inspired heavily by Oliver Bramley's run script for MCE/CCS
+# 
+#   Written to make submission of jobs to the HPC easier and quicker
+#     
+#
+#########################################################################################
+import sys
+import socket
+import os
+import subprocess
+import getpass
+import random
+import shutil
+import glob
+import csv
+
+
+#########################################################################################
+#                              VARIABLES TO SET FOR SIMULATION                          #
+#########################################################################################
+
+# Number of repeats 
+repeats=64
+# # Number of nodes/folders
+nodes=1
+# #Number of parallel cores per folder/node (max 8)
+cores=8
+# Name of running folder 
+# Default : <method>-<system>-<random number> ie CCS-HP-31254
+# Otherwise:  <method>-<system>-<runfolder string>
+Runfolder='Propylene'
+# Restart Flag 
+restart = 'NO'
+# Geometry flag
+Geom = 1
+
+#########################################################################################
+#                                   END OF INPUTS                                       #
+#########################################################################################
+#                * NO NEED TO SCROLL FURTHER IF USING AS BLACKBOX *                     #
+#########################################################################################
+
+
+if __name__=="__main__":
+    #Check basic arguements
+    if(isinstance(repeats,int)==False):
+        sys.exit("Number of repeats must be an integer")
+    elif(isinstance(nodes,int)==False):
+        sys.exit("Number of folders must be an integer")
+    elif(isinstance(cores,int)==False):
+        sys.exit("Number of parallel cores must be an integer")
+    elif(repeats<1):
+        sys.exit("Not enough runs selected. Must be 1 or greater")
+    elif(nodes<1):
+        sys.exit("Not enough nodes selected. Must be 1 or greater")
+    elif(nodes>100):
+        sys.exit("Too many nodes. Maximum of 100 simultaneous submisions")
+    elif(cores>8):
+        sys.exit("Too many cores selected. Maximum of 8 available")
+    elif(cores<1):
+        sys.exit("Not enough cores selected. Must be 1 or greater")
+    elif((repeats/nodes)>5000):
+        sys.exit("Too many repeats per folder. Must be less than 500")
+
+    if(restart=="NO"):
+        if((repeats%(nodes*cores))!=0):
+            sys.exit("Number of repeats must be an integer multiple of cores*folders")
+        elif(nodes*cores>101):
+            sys.exit("Total number of cores should stay below 100")
+        elif(Geom not in{0,1}):
+            sys.exit("Geometry flag msut be zero or 1")
+        else:
+            print("Arguments checked")
+            Hostname=socket.gethostname()
+            if(Hostname==("login1.arc4.leeds.ac.uk")or(Hostname==("login2.arc4.leeds.ac.uk"))):
+                HPCFLG=1
+            elif(Hostname==("login1.arc3.leeds.ac.uk")or(Hostname==("login2.arc3.leeds.ac.uk"))):
+                HPCFLG=1 
+            else:
+                HPCFLG=1
+        
+        #Might need grid altering calibration test for chlin451 bash code
+        #if [[ -n $( echo $HOSTNAME | fgrep -e "chmlin451" ) ]]; then
+        #grdalt=1
+        #else
+        #grdalt=0
+        #fi
+
+        #Makes execution folder and run folder
+        if(HPCFLG==1):
+            if not os.path.exists("../EXEC"):
+                os.mkdir("../EXEC")
+            EXDIR="../EXEC"
+        else:
+            # subprocess.run(['module','load','mkl'])
+            os.environ['LOGNAME']
+            EXDIR="/nobackup/"+getpass.getuser()
+
+
+        if os.path.exists(EXDIR+"/"+Runfolder):
+            value=input("File already exists do you want to delete it? y/n\n")
+            if(value=='y'):
+                shutil.rmtree(EXDIR+"/"+Runfolder)
+            else:
+                sys.exit("Runfolder already exists. Change the Runfolder name or delte/move it")
+        
+        os.mkdir(EXDIR+"/"+Runfolder)
+        
+        EXDIR1=EXDIR+"/"+Runfolder  
+
+        mcerunf=os.getcwd()
+        # result=open(EXDIR1+"/result.sh","w")
+        # result.write("python "+mcerunf+"/collate.py $PWD "+(str(repeats))+" "+str(nodes)+" '"+Runfolder+"' "+(str(HPCFLG))+" '"+prop+"'")
+        # result.close()
+        # subprocess.run(['chmod', 'u+x', EXDIR1+'/result.sh'])
+            
+        #Copies input files
+        shutil.copy2("run.py",EXDIR1)
+ 
+
+        
+
+        
+        #Makes the program input file
+        # with open('rundata.csv','w',newline='')as file:
+        #         writer = csv.writer(file)
+        #         writer.writerow([gen,prop,restart,inputs.cmprss,inputs.method,int(repeats/nodes),inputs.Conjugate_Repeats])
+        #         writer.writerow(inputs.systems.values())
+        #         writer.writerow(inputs.parameters.values())
+        #         writer.writerow(inputs.Train.values())
+        #         writer.writerow(inputs.clone.values())
+        #         writer.writerow(inputs.paramz.values())
+        #         writer.writerow(inham.EL.values())
+        #         writer.writerow(inputs.prop.values())
+        #         if(inputs.systems['System']=='MP'):
+        #             writer.writerow(inham.MP.values())
+        #         elif(inputs.systems['System']=='HP'):
+        #             writer.writerow(inham.HP.values())
+        #         else:
+        #             writer.writerow(inham.SB.values())
+        # shutil.copy2("rundata.csv",EXDIR1)
+        
+
+        for i in range(nodes):
+            path=os.path.join(EXDIR1,"run-"+str(i+1))
+            os.mkdir(EXDIR1+"/run-"+str(i+1))
+            
+
+        #Selects the right make file and executes
+        # os.chdir("../build")
+        # if(HPCFLG==1):
+        #     shutil.copy2("../build/makefile_arc","../build/Makefile")
+        #     subprocess.run(["make"])
+        # else:
+        #     shutil.copy2("../build/makefile_chmlin","../build/Makefile")
+        #     subprocess.run(["make"])
+        # shutil.copy2("MCE.exe",EXDIR1)
+        # shutil.copy2("interpolate.exe",EXDIR1)
+        # shutil.copy2("subavrg.exe",EXDIR1)
+        
+
+        # if(inputs.systems['freqflg']==1):
+        #     if os.path.exists(mcerunf+"/freq.dat"):
+        #         shutil.copy2(mcerunf+"/freq.dat",EXDIR1)
+        #     else:
+        #         subprocess.run(["./integrator.exe"])
+        #         shutil.copy2("freq.dat",EXDIR1)
+
+        os.chdir(EXDIR1)
+        EXDIR1=os.getcwd()
+
+       
+
+
+        # for i in range (nodes):
+        #     shutil.copy2("MCE.exe","run-"+str(i+1))
+        #     shutil.copy2("rundata.csv","run-"+str(i+1))
+        #     if(Geom==1):
+        #         for k in range(repeats/nodes):
+        #             shutil.copy2("freq.dat","/run-"+str(i+1)+"/freq"+str(k+1)+".dat")
+        
+    #If on a SGE machine make job submission file
+    if(HPCFLG==1):
+        number=random.randint(99999,1000000)
+        file1="Plasma"+str(number)+".sh"
+        f=open(file1,"w")
+        f.write("#$ -cwd -V \n")
+        f.write("#$ -l  h_vmem=1G, h_rt=48:00:00 \n")
+        f.write("#$ -pe smp "+str(cores)+" \n") #Use shared memory parallel environemnt 
+
+        f.write("#$ -t 1-"+str(repeats)+" \n")
+        f.write("module load mkl \n")
+        f.write("module load test qchem \n")
+        f.write("cd "+EXDIR1+"/run-$SGE_TASK_ID/ \n")
+        f.write("echo "'"Running on $HOSTNAME in folder $PWD" \n')
+        f.write("cp Inputs/* t${SGE_TASK_ID}")
+        f.write("echo ' ' >> t.ini \n")
+        f.write("echo '1.0 ' >> t.ini \n")
+        f.write("echo '0.0' >> t.ini \n") 
+        f.write("echo ' ' >> t.ini \n")
+        f.write("cp t.ini t.0 \n")
+        f.write(" ../Code/prop.sh "+str(cores) +"\n")
+    
+        f.close()
+        # if(cores!=1):
+        #     os.environ["OMP_NUM_THREADS"]=str(cores)
+        # subprocess.call(['qsub',file1])
+
+    else:
+        print('Probably dont run this here')
+        
+
+    
